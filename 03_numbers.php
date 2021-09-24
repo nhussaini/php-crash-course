4bd8e282b45837e1a72fb6600bc239aa6ad0d7ef
nhussaini <nasrullah_hussaini@yahoo.com>
2021-09-23 23:43:13 -0400

    numbers and its related function in php

--- 03_numbers.php
+++ 03_numbers.php
@@ -1,8 +1,13 @@
 <?php
 
 // Declaring numbers
+$a = 5;
+$b = 4;
+$c = 1.2;
+
 
 // Arithmetic operations
+echo $a + $b;
 
 // Assignment with math operators
 
@@ -11,11 +16,21 @@
 // Decrement operator
 
 // Number checking functions
+is_int(5); //true
+is_numeric("3.45"); //true// it converts the number which is as string to an integer
+is_numeric("3g.45"); //false;
 
 // Conversion
-
+$strNumber = '12.34';
+$number = (int)$strNumber;
+var_dump($number);
+echo '<br>';
 // Number functions
+echo "abs(-15)=" . abs(-15) . '<br>';
 
 // Formatting numbers
+
+$number = 123456789.12345;
+echo number_format($number, 2);
 
 // https://www.php.net/manual/en/ref.math.php
